2f03c46cd8743b22a54396dc7299336a087013a2
lhgoulart <lhgoulart92@yahoo.com.br>
2019-01-18 11:33:17 -0200

    Acrescentando comentarios aos posts

--- src/main/java/com/lhgoulart/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lhgoulart/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lhgoulart.workshopmongo.domain.Post;
 import com.lhgoulart.workshopmongo.domain.User;
 import com.lhgoulart.workshopmongo.dto.AuthorDTO;
+import com.lhgoulart.workshopmongo.dto.CommentDTO;
 import com.lhgoulart.workshopmongo.repository.PostRepository;
 import com.lhgoulart.workshopmongo.repository.UserRepository;
 
@@ -39,7 +40,14 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		CommentDTO c1 = new CommentDTO("Boa viagem", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+				
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz!", new AuthorDTO(maria));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia", sdf.parse("24/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
