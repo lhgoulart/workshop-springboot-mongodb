b5b64ae439d08412d9673e9ac8398d136ea5efa3
lhgoulart <lhgoulart92@yahoo.com.br>
2019-01-18 09:31:55 -0200

    Operacao da instanciacao da base de dados

---
+++ src/main/java/com/lhgoulart/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.lhgoulart.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.lhgoulart.workshopmongo.domain.User;
+import com.lhgoulart.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
