8d9c3061119a45ebe9fe11b3bfa45023f1c79140
lhgoulart <lhgoulart92@yahoo.com.br>
2019-01-18 10:01:23 -0200

    Obtendo um usuario por id

--- src/main/java/com/lhgoulart/workshopmongo/services/UserService.java
+++ src/main/java/com/lhgoulart/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.lhgoulart.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lhgoulart.workshopmongo.domain.User;
 import com.lhgoulart.workshopmongo.repository.UserRepository;
+import com.lhgoulart.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
 	}
 }
