2caf20b7d2928c9a9b43d84b89f8b822637d6597
lhgoulart <lhgoulart92@yahoo.com.br>
2019-01-18 10:59:38 -0200

    Projecao dos dados do autor com dto

--- src/main/java/com/lhgoulart/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lhgoulart/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.lhgoulart.workshopmongo.domain.Post;
 import com.lhgoulart.workshopmongo.domain.User;
+import com.lhgoulart.workshopmongo.dto.AuthorDTO;
 import com.lhgoulart.workshopmongo.repository.PostRepository;
 import com.lhgoulart.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
